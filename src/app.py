8a9685a562458c1719bd445ce39c13d85bf9bbe6
alejandro <dev@alejandro-ao.com>
2024-02-29 12:00:12 +0100

    update title

--- src/app.py
+++ src/app.py
@@ -9,8 +9,8 @@ from langchain_core.prompts import ChatPromptTemplate
 load_dotenv()
 
 # app config
-st.set_page_config(page_title="Chat with websites", page_icon="🤖")
-st.title("Chat with websites")
+st.set_page_config(page_title="Streaming bot", page_icon="🤖")
+st.title("Streaming bot")
 
 def get_response(user_query, chat_history):
 
